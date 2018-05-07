20157b99fd952a004429c9c4d7d49b85da267f93
Elke <elke.renard@gmail.com>
2018-05-07 19:14:34 +0200

    structure website

--- CloudAPI/src/app/app.component.ts
+++ CloudAPI/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ListAllComponent } from './list-all/list-all.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { DetailComponent } from './detail/detail.component';
 
 @Component({
   selector: 'app-root',
--- CloudAPI/src/app/list-all/list-all.component.ts
+++ CloudAPI/src/app/list-all/list-all.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-all',
+  selector: 'list-all',
   templateUrl: './list-all.component.html',
   styleUrls: ['./list-all.component.scss']
 })
