4a6b35083a51715dd329e19b2d49391557a53353
Elke <elke.renard@gmail.com>
2018-05-20 12:13:03 +0200

    lijst regions

--- CloudAPI/src/app/regions/regions.component.ts
+++ CloudAPI/src/app/regions/regions.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-regions',
+  selector: 'regions',
   templateUrl: './regions.component.html',
   styleUrls: ['./regions.component.scss']
 })
