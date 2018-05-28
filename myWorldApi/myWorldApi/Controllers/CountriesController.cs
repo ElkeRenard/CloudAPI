447980b86ddb63dcfa174eb1b9922ed593114322
Elke <elke.renard@gmail.com>
2018-05-28 14:21:30 +0200

    filter on part of name is working o website

--- myWorldApi/myWorldApi/Controllers/CountriesController.cs
+++ myWorldApi/myWorldApi/Controllers/CountriesController.cs
@@ -20,14 +20,15 @@ namespace myWorldApi.Controllers
             this.context = context;
         }
 
-        [HttpGet]
-        
-        public List<Country> getAll()
+        [HttpGet]        
+        public List<Country> getAll(string name)
         {
-            return context.Countries.ToList();
+            IQueryable<Country> query = context.Countries;
+            if (!string.IsNullOrWhiteSpace(name))
+                query = query.Where(d => d.Name.Contains(name));
+            return query.ToList();
         }
 
-        [EnableCors("AllowAll")]
         [HttpGet]
         [Route("{id}")]
         public IActionResult getCountryById(long id)
@@ -38,15 +39,6 @@ namespace myWorldApi.Controllers
             return Ok(country);
         }
 
-        /*[HttpGet]
-        [Route("{name}")]
-        public List<Country> searchCountries([FromUri]string name) { 
-            IQueryable<Country> query = context.Countries;
-
-            if (!string.IsNullOrWhiteSpace(name))
-                query = query.Where(d => d.Name == name);
-            return query.ToList();
-        }*/
 
         [HttpDelete]
         [Route("{id}")]
@@ -63,20 +55,22 @@ namespace myWorldApi.Controllers
         [HttpPost]
         public IActionResult addCountry([FromBody] Country newCountry)
         {
+            newCountry.Favourite = false;
             context.Countries.Add(newCountry);
             context.SaveChanges();
             return Created("", newCountry);
         }
 
-       /* [HttpPut]
-        public IActionResult updateCountry([FromBody] Country countryIn, [FromBody] Boolean favorite)
+       [HttpPut]
+        public IActionResult updateCountry([FromBody] Country countryIn)
         {
 
             var country = context.Countries.Find(countryIn.id);
             if (country == null)
                 return NotFound();
+            country.Favourite = countryIn.Favourite;
             context.SaveChanges();
             return Ok(country);
-        }*/
+        }
     }
 }
