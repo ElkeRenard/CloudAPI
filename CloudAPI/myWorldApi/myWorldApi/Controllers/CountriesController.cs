720dfe926dc3ab7d6f75317c8ce866991c64a475
Elke <elke.renard@gmail.com>
2018-05-28 09:11:49 +0200

    ownAPI get and add new works in website

---
+++ CloudAPI/myWorldApi/myWorldApi/Controllers/CountriesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using myWorldApi.models;
+
+namespace myWorldApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Countries")]
+    public class CountriesController : Controller
+    {
+        private readonly WorldContext context;
+
+        public CountriesController(WorldContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        
+        public List<Country> getAll()
+        {
+            return context.Countries.ToList();
+        }
+
+        [EnableCors("AllowAll")]
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult getCountryById(long id)
+        {
+            var country = context.Countries.Find(id);
+            if (country == null)
+                return NotFound();
+            return Ok(country);
+        }
+
+        /*[HttpGet]
+        [Route("{name}")]
+        public List<Country> searchCountries([FromUri]string name) { 
+            IQueryable<Country> query = context.Countries;
+
+            if (!string.IsNullOrWhiteSpace(name))
+                query = query.Where(d => d.Name == name);
+            return query.ToList();
+        }*/
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult deleteCountry(long id)
+        {
+            var country = context.Countries.Find(id);
+            if (country == null)
+                return NotFound();
+            context.Countries.Remove(country);
+            context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult addCountry([FromBody] Country newCountry)
+        {
+            context.Countries.Add(newCountry);
+            context.SaveChanges();
+            return Created("", newCountry);
+        }
+
+       /* [HttpPut]
+        public IActionResult updateCountry([FromBody] Country countryIn, [FromBody] Boolean favorite)
+        {
+
+            var country = context.Countries.Find(countryIn.id);
+            if (country == null)
+                return NotFound();
+            context.SaveChanges();
+            return Ok(country);
+        }*/
+    }
+}
---
+++ CloudAPI/myWorldApi/myWorldApi/models/WorldContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myWorldApi.models
+{
+    public class WorldContext: DbContext
+    {
+        public WorldContext(DbContextOptions<WorldContext> options): base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+
+    
+}
